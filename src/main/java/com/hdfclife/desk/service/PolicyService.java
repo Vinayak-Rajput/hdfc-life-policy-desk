fd6c4260cce3b85d6f9c38b00b547e70e0e3efbb
vinayak-rajput <vk.ra.org@gmail.com>
2026-09-08 15:40:21 +0530

    Provided reference for PolicyStore interface in PolicyService class

--- src/main/java/com/hdfclife/desk/service/PolicyService.java
+++ src/main/java/com/hdfclife/desk/service/PolicyService.java
@@ -1,7 +1,14 @@
 package com.hdfclife.desk.service;
 
+import com.hdfclife.desk.store.PolicyStore;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PolicyService {
+
+    PolicyStore policyStore;
+
+    public PolicyService(PolicyStore policyStore) {
+        this.policyStore = policyStore;
+    }
 }

142e8d188c242892aab4b71e56268477f065fde6
vinayak-rajput <vk.ra.org@gmail.com>
2026-09-08 15:43:11 +0530

    Enabled the configuration properties defined in HdfcProperties class for our spring application

--- src/main/java/com/hdfclife/desk/DeskApplication.java
+++ src/main/java/com/hdfclife/desk/DeskApplication.java
@@ -1,9 +1,12 @@
 package com.hdfclife.desk;
 
+import com.hdfclife.desk.config.HdfcProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(HdfcProperties.class)
 public class DeskApplication {
 
     public static void main(String[] args) {

e9ac50ad7b761d2c593cf37773334f30f9215381
vinayak-rajput <vk.ra.org@gmail.com>
2026-09-08 15:59:05 +0530

    Created Entities Policy, Claim and Urgency

---
+++ src/main/java/com/hdfclife/desk/model/Claim.java
@@ -0,0 +1,41 @@
+package com.hdfclife.desk.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Claim {
+
+    private String policyNo;
+    private Double claimAmount;
+    private Urgency urgency;
+
+    public Claim(String policyNo, Double claimAmount, Urgency urgency) {
+        this.policyNo = policyNo;
+        this.claimAmount = claimAmount;
+        this.urgency = urgency;
+    }
+
+    public String getPolicyNo() {
+        return policyNo;
+    }
+
+    public void setPolicyNo(String policyNo) {
+        this.policyNo = policyNo;
+    }
+
+    public Double getClaimAmount() {
+        return claimAmount;
+    }
+
+    public void setClaimAmount(Double claimAmount) {
+        this.claimAmount = claimAmount;
+    }
+
+    public Urgency getUrgency() {
+        return urgency;
+    }
+
+    public void setUrgency(Urgency urgency) {
+        this.urgency = urgency;
+    }
+}
---
+++ src/main/java/com/hdfclife/desk/model/Urgency.java
@@ -0,0 +1,8 @@
+package com.hdfclife.desk.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public enum Urgency {
+    HIGH, MEDIUM, LOW
+}

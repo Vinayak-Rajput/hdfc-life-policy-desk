5e7cecb1bfa66cf55e3bd068dfba8ad4e18aea12
vinayak-rajput <vk.ra.org@gmail.com>
2026-09-10 22:33:24 +0530

    Updated Claim model to satisy Request and Response entities.

--- src/main/java/com/hdfclife/desk/model/Claim.java
+++ src/main/java/com/hdfclife/desk/model/Claim.java
@@ -2,14 +2,26 @@ package com.hdfclife.desk.model;
 
 public class Claim {
 
+    private String claimNo;
     private String policyNo;
     private Double claimAmount;
     private Urgency urgency;
+    private String status;
 
-    public Claim(String policyNo, Double claimAmount, Urgency urgency) {
+    public Claim(String claimNo,String policyNo, Double claimAmount, Urgency urgency, String status) {
+        this.claimNo = claimNo;
         this.policyNo = policyNo;
         this.claimAmount = claimAmount;
         this.urgency = urgency;
+        this.status = status;
+    }
+
+    public String getClaimNo() {
+        return claimNo;
+    }
+
+    public void setClaimNo(String claimNo) {
+        this.claimNo = claimNo;
     }
 
     public String getPolicyNo() {
@@ -34,5 +46,13 @@ public class Claim {
 
     public void setUrgency(Urgency urgency) {
         this.urgency = urgency;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }

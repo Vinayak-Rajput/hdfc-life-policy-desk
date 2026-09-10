22f07ab06e7d7e5b820d40c12653d1ef61435242
vinayak-rajput <vk.ra.org@gmail.com>
2026-09-10 16:50:38 +0530

    Implemented EndPoints for PolicyController & ClaimController starting with "/api/policies" and "/api/claims"

--- src/main/java/com/hdfclife/desk/web/ClaimController.java
+++ src/main/java/com/hdfclife/desk/web/ClaimController.java
@@ -1,7 +1,33 @@
 package com.hdfclife.desk.web;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.hdfclife.desk.model.Claim;
+import com.hdfclife.desk.service.ClaimService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/claims")
 public class ClaimController {
+
+    ClaimService claimService;
+    public ClaimController(ClaimService claimService) {
+        this.claimService = claimService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Claim> createClaim(@RequestBody Claim claim) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(claim);
+    }
+
+    @GetMapping("/{claimNo}")
+    public ResponseEntity<Claim> getClaimByNo(@PathVariable String claimNo) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(claimService.getClaimByNo(claimNo));
+    }
 }
--- src/main/java/com/hdfclife/desk/web/PolicyController.java
+++ src/main/java/com/hdfclife/desk/web/PolicyController.java
@@ -1,7 +1,78 @@
 package com.hdfclife.desk.web;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.hdfclife.desk.model.Claim;
+import com.hdfclife.desk.model.Policy;
+import com.hdfclife.desk.service.PolicyService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/policies")
 public class PolicyController {
+
+    PolicyService policyService;
+
+    public PolicyController(PolicyService policyService) {
+        this.policyService = policyService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Policy>> getPolicies() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ArrayList<>());
+    }
+
+    @GetMapping("/{policyNo}")
+    public ResponseEntity<Policy> getPolicyByNo(@PathVariable("policyNo") String policyNo) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(policyService.getPolicyByNo(policyNo));
+    }
+
+    @GetMapping("?status=Active")
+    public ResponseEntity<List<Policy>> getPoliciesByStatus(@RequestParam("status") String status) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(policyService.getPoliciesByStatus(status));
+    }
+
+    @GetMapping("?type=TERM")
+    public ResponseEntity<List<Policy>> getPoliciesByType(@RequestParam("type") String type) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(policyService.getPoliciesByType(type));
+    }
+
+    @PostMapping
+    public ResponseEntity<Policy> createPolicy(@RequestBody Policy policy) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(policyService.createPolicy(policy));
+    }
+
+    @PutMapping("/{policyNo}")
+    public ResponseEntity<Policy> updatePolicy(@PathVariable("policyNo") String policyNo) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(policyService.updatePolicy(policyNo));
+    }
+
+    @DeleteMapping("/{policyNo}")
+    public ResponseEntity<Policy> deletePolicy(@PathVariable("policyNo") String policyNo) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(policyService.deletePolicy(policyNo));
+    }
+
+    @GetMapping("/{policyNo}/claims")
+    public ResponseEntity<List<Claim>> getClaimsByNo(@PathVariable("policyNo") String policyNo) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(policyService.getClaimsByPolicyNo(policyNo));
+    }
 }

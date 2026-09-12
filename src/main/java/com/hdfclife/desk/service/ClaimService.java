afdbac6ce22e26a252a5986b2b70846676ea79c8
vinayak-rajput <vk.ra.org@gmail.com>
2026-09-12 14:52:40 +0530

    Updated Claim and Policy Services

--- src/main/java/com/hdfclife/desk/service/ClaimService.java
+++ src/main/java/com/hdfclife/desk/service/ClaimService.java
@@ -1,7 +1,52 @@
 package com.hdfclife.desk.service;
 
+import com.hdfclife.desk.exception.ClaimNotFoundException;
+import com.hdfclife.desk.model.Claim;
+import com.hdfclife.desk.store.InMemoryPolicyStore;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ClaimService {
+
+    private final InMemoryPolicyStore policyStore;
+
+    public ClaimService() {
+        this.policyStore = new InMemoryPolicyStore();
+    }
+
+    public Claim createClaim(Claim claim) {
+
+        return policyStore.addClaim(claim);
+    }
+    public Claim getClaimByNo(String claimNo) {
+
+        Claim claim = policyStore.findClaimByClaimNo(claimNo);
+
+        if(claim == null) {
+
+            throw new ClaimNotFoundException("Claim with No. " + claimNo + " doesn't exist.");
+        }
+
+        return claim;
+    }
+
+    public List<Claim> getClaimsByPolicyNo(String policyNo) {
+
+        List<Claim> claims = policyStore.findAllClaims();
+
+        List<Claim> filteredClaims = new ArrayList<>();
+
+        for(Claim claim : claims){
+
+            if(claim.getPolicyNo().equals(policyNo)){
+
+                filteredClaims.add(claim);
+            }
+        }
+
+        return filteredClaims;
+    }
 }
--- src/main/java/com/hdfclife/desk/service/PolicyService.java
+++ src/main/java/com/hdfclife/desk/service/PolicyService.java
@@ -1,7 +1,13 @@
 package com.hdfclife.desk.service;
 
+import com.hdfclife.desk.exception.DuplicatePolicyException;
+import com.hdfclife.desk.exception.PolicyNotFoundException;
+import com.hdfclife.desk.model.Policy;
 import com.hdfclife.desk.store.PolicyStore;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PolicyService {
@@ -10,5 +16,89 @@ public class PolicyService {
 
     public PolicyService(PolicyStore policyStore) {
         this.policyStore = policyStore;
+    }
+
+    public Policy createPolicy(Policy policy) {
+
+        Policy createdPolicy = policyStore.add(policy);
+
+        if(createdPolicy == null){
+
+            throw new DuplicatePolicyException("Policy already exists");
+        }
+
+        return createdPolicy;
+    }
+
+    public Policy getPolicyByNo(String policyNo) {
+
+        Policy policy = policyStore.findByPolicyNo(policyNo);
+
+        if(policy == null) {
+
+            throw new PolicyNotFoundException("Policy with No. " + policyNo + " doesn't exist.");
+        }
+        return policy;
+    }
+
+    public Policy updatePolicy(String policyNo, Policy policy) {
+
+        if(policyStore.findByPolicyNo(policyNo) == null) {
+
+            throw new PolicyNotFoundException("Policy with No. " + policyNo + " doesn't exist.");
+        }
+
+        Policy updatedPolicy = policyStore.update(policyNo, policy);
+        return policy;
+
+    }
+
+    public Policy deletePolicy(String policyNo) {
+
+        Policy policy = policyStore.findByPolicyNo(policyNo);
+
+        if(policy == null) {
+
+            throw new PolicyNotFoundException("Policy with No. " + policyNo + " doesn't exist.");
+        }
+
+        policyStore.delete(policyNo);
+
+        return policy;
+    }
+
+    public List<Policy> getPoliciesByStatus(String status) {
+
+        List<Policy> requiredPolicies = new ArrayList<>();
+
+        for(Policy policy : policyStore.findAll()) {
+
+            if(policy.getStatus().equals(status)) {
+
+                requiredPolicies.add(policy);
+            }
+        }
+
+        return requiredPolicies;
+    }
+
+    public List<Policy> getPoliciesByType(String type) {
+
+        List<Policy> requiredPolicies = new ArrayList<>();
+
+        for(Policy policy : policyStore.findAll()) {
+
+            if(policy.getType().equals(type)) {
+
+                requiredPolicies.add(policy);
+            }
+        }
+
+        return requiredPolicies;
+    }
+
+    public List<Policy> getPolicies() {
+
+        return policyStore.findAll();
     }
 }
